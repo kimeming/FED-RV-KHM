28c68735b069e2bedd6d557e983c491927698abb
kimeming <kimeming030@gmail.com>
2024-11-15 13:20:12 +0900

    작업중

--- personal/haribo/js/main.js
+++ personal/haribo/js/main.js
@@ -1,46 +1,47 @@
 $(document).ready(function () {
-    gsap.registerPlugin(ScrollTrigger);
+  // main jelly scroll ani
+  gsap.registerPlugin(ScrollTrigger);
 
-    let container = document.getElementById("container");
-    gsap.to(container, {
-      x: () => `${-(container.scrollWidth - document.documentElement.clientWidth)}px`,
-      ease: "none",
-      scrollTrigger: {
-        trigger: container,
-        invalidateOnRefresh: true,
-        pin: true,
-        scrub: 1,
-        start: "top top",
-        end: () => `+=${container.scrollWidth - document.documentElement.clientWidth}`
-        }
-    });
-    
-    const scrollColorElems = document.querySelectorAll(".horizontal-container");
+  let container = document.getElementById("container");
+  gsap.to(container, {
+    x: () => `${-(container.scrollWidth - document.documentElement.clientWidth)}px`,
+    ease: "none",
+    scrollTrigger: {
+      trigger: container,
+      invalidateOnRefresh: true,
+      pin: true,
+      scrub: 1,
+      start: "top top",
+      end: () => `+=${container.scrollWidth - document.documentElement.clientWidth}`
+      }
+  });
+  
+  const scrollColorElems = document.querySelectorAll(".horizontal-container");
 
-    scrollColorElems.forEach((colorSection, i) => {
-        let horizontalWidth = $('.horizontal-container').outerWidth();
-        let horizontalWidthMultiplied = horizontalWidth*5;
-        
-        const myTimeline = gsap.timeline({
-    
-            scrollTrigger: {
-                trigger: ".horizontal-container",
-                pin: false,
-                scrub: true,
-                start: "top",   
-                end: () => `+=${horizontalWidthMultiplied}`
-            }
+  scrollColorElems.forEach((colorSection, i) => {
+      let horizontalWidth = $('.horizontal-container').outerWidth();
+      let horizontalWidthMultiplied = horizontalWidth*5;
+      
+      const myTimeline = gsap.timeline({
+  
+          scrollTrigger: {
+              trigger: ".horizontal-container",
+              pin: false,
+              scrub: true,
+              start: "top",   
+              end: () => `+=${horizontalWidthMultiplied}`
+          }
 
-        });
+      });
 
-        myTimeline
-         .to('.section-bg', { duration: 1, backgroundColor: '#db0a19', ease: "none" }, 0)
-         .to('.section-bg', { duration: 1, backgroundColor: '#e78508', ease: "none" }, 1)
-         .to('.section-bg', { duration: 1, backgroundColor: '#58a425', ease: "none" }, 2)
-         .to('.section-bg', { duration: 1, backgroundColor: '#0079bd', ease: "none" }, 3)
-         .to('.section-bg', { duration: 1, backgroundColor: '#00216e', ease: "none" }, 4)
-         .to('.bg-text', { duration: 1, color: '#FA9570', ease: "none" }, 0)
-         .to('.bg-text', { duration: 1, color: '#de9f0d', ease: "none" }, 1)
+      myTimeline
+       .to('.section-bg', { duration: 1, backgroundColor: '#db0a19', ease: "none" }, 0)
+       .to('.section-bg', { duration: 1, backgroundColor: '#e78508', ease: "none" }, 1)
+       .to('.section-bg', { duration: 1, backgroundColor: '#58a425', ease: "none" }, 2)
+       .to('.section-bg', { duration: 1, backgroundColor: '#0079bd', ease: "none" }, 3)
+       .to('.section-bg', { duration: 1, backgroundColor: '#00216e', ease: "none" }, 4)
+       .to('.bg-text', { duration: 1, color: '#FA9570', ease: "none" }, 0)
+       .to('.bg-text', { duration: 1, color: '#de9f0d', ease: "none" }, 1)
     });
 
     let rotate = gsap.timeline({
