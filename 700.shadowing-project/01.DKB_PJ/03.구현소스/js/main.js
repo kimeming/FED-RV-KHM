664300f1d9959bbba0e8200aeac4505cc77177b1
kimeming <kimeming030@gmail.com>
2024-12-16 15:54:57 +0900

    도깨비 PJ 소스 업데이트

--- 700.shadowing-project/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.shadowing-project/01.DKB_PJ/03.구현소스/js/main.js
@@ -3,6 +3,24 @@
 // 메인배너 슬라이드 기능함수 불러오기
 import slideFn from "./slide_fn.js";
 
-// console.log(slideFn);
-// 슬라이드함수 호출하여 실행하기
+// 도깨비PJ 데이터 불러오기
+import {previewData} from "../data/dkb_data.js";
+
+// console.log(previewData);
+// 1. 슬라이드함수 호출하여 실행하기
 slideFn();
+
+// 2. 데이터 세팅하기
+// (1) 미리보기 데이터 세팅하기
+// 대상: preview-box>ul
+// 데이터: previewData
+// map메서드 사용하기 배열값 변형출력? map&join
+
+$('.preview-box>ul').html(
+    previewData.map(v => `
+        <li data-idx="${v.idx}">
+            <h3>${v.title}</h3>
+            <p>${v.story}</p>
+        </li>
+    `)
+);
