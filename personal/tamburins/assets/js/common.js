e99c34333e702f1d41a2cf661e96382bb823df2e
kimeming <kimeming030@gmail.com>
2025-01-15 11:25:46 +0900

    뷰 컴포넌트 연습중

---
+++ personal/tamburins/assets/js/common.js
@@ -0,0 +1,14 @@
+// js/main.js
+import HeaderComponent from "./component/headerComp.js";
+import MainComponent from "./component/mainComp.js";
+import FooterComponent from "./component/footerComp.js";
+
+// 전역 등록
+Vue.component("header-component", HeaderComponent);
+Vue.component("main-component", MainComponent);
+Vue.component("footer-component", FooterComponent);
+
+// Vue 인스턴스 초기화
+new Vue({
+  el: "#app",
+});
