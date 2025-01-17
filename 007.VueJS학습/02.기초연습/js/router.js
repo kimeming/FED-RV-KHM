f224312e9685ca222a8518a09570cb17461160f8
kimeming <kimeming030@gmail.com>
2025-01-17 14:35:26 +0900

    뷰라우터js생성

---
+++ 007.VueJS학습/02.기초연습/js/router.js
@@ -0,0 +1,14 @@
+// 뷰 라우터 세팅 js router.js
+
+/*
+    [ 뷰 라우터 인스턴스 내보내기 ]
+    라우터 세팅 방식
+    new VueRouter({routes: []})
+    -> 만약 외부에 세팅될 경우
+    const routes = []
+    new VueRouter({routes})
+    => routes라는 속성명은 정해진 이름임
+*/
+export default new VueRouter(
+    {},
+);
---
+++ 007.VueJS학습/02.기초연습/js/vue_router.js
@@ -0,0 +1,4 @@
+// vue_router.js
+
+// 라우터 세팅 js 불러오기
+import router from './router.js'
