cfa009d8f5d89c4b13d892000e71f0083fe302ad
kimeming <kimeming030@gmail.com>
2025-01-21 17:43:59 +0900

    vogue footer component 연결

--- 700.shadowing-project/02.VOGUE_PJ/03.구현소스/js/components/common.js
+++ 700.shadowing-project/02.VOGUE_PJ/03.구현소스/js/components/common.js
@@ -1,9 +1,9 @@
 // vogue PJ 공통 컴포넌트 common.js
 
 // 1. header component
-const TopComp = Vue.component('top-comp', {
-    // 1-1. 템플릿
-    template: `
+const TopComp = Vue.component("top-comp", {
+  // 1-1. 템플릿
+  template: `
       <header class="top-area inbox">
         <!-- 1-1. 로고박스 -->
         <div class="logo">
@@ -28,15 +28,51 @@ const TopComp = Vue.component('top-comp', {
       </header>
     
     `,
-    // 1-2. 데이터 셋업 리턴 메서드
-    data(){
-        return{
-            // (1) GNB 메뉴 데이터
-            gnbMenu: ['FASHION', 'BEAUTY', 'LIFESTYLE', 'CULTURE', 'VIDEO'],
-            sumMenu: ['KOREA', '구독하기', '≡'],
-        };
-    }
+  // 1-2. 데이터 셋업 리턴 메서드
+  data() {
+    return {
+      // (1) GNB 메뉴 데이터
+      gnbMenu: ["FASHION", "BEAUTY", "LIFESTYLE", "CULTURE", "VIDEO"],
+      sumMenu: ["KOREA", "구독하기", "≡"],
+    };
+  },
 });
-const BottomComp = Vue.component('bottom-comp', {});
+const BottomComp = Vue.component("bottom-comp", {
+  template: `
+  <div id="footer-area">
+        <footer class="footer-area ibx common-area">
+          <!-- 3-1.하단로고 -->
+          <div class="blogo">
+            <img src="./images/svg/logo_white_m.svg" alt="하단로고" />
+          </div>
+          <!-- 3-2.하단링크 --> 
+          <ul class="blink">
+            <li>
+              <a href="#">정기구독</a>
+            </li>
+            <li>
+              <a href="#">회사소개</a>
+            </li>
+            <li>
+              <a href="#">광고 및 제휴</a>
+            </li>
+            <li>
+              <a href="#">개인정보 처리방침</a>
+            </li>
+          </ul>
+          <!-- 3-3.회사주소 -->
+          <address class="addr">
+            WWW.VOGUE.COM Ⓒ CONDÉNAST ASIA PACIFIC. INC. ALL RIGHTS RESERVED.
+            VOGUE.COM KOREA IS OPERATED BY DOOSAN MAGAZINE.
+          </address>
+         
+        </footer>
+      </div>
+      <!-- 위로가기버튼 -->
+      <a href="#" class="tbtn fi fi-angle-up">
+        <span class="ir">위로가기버튼</span>
+      </a>
+     `,
+});
 
-export {TopComp, BottomComp}
+export { TopComp, BottomComp };
