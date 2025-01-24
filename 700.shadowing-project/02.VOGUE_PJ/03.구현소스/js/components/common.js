0d3a6db512bde5495a113d340332bf9d75b00db0
kimeming <kimeming030@gmail.com>
2025-01-24 12:00:57 +0900

    소스업데이트

--- 700.shadowing-project/02.VOGUE_PJ/03.구현소스/js/components/common.js
+++ 700.shadowing-project/02.VOGUE_PJ/03.구현소스/js/components/common.js
@@ -1,9 +1,9 @@
 // 보그 PJ 공통 컴포넌트 - common.js
 
 // 1. 상단컴포넌트
-const TopComp = Vue.component('top-comp',{
-    // 1-1. 템플릿코드설정 /////
-    template:`  
+const TopComp = Vue.component("top-comp", {
+  // 1-1. 템플릿코드설정 /////
+  template: `  
     <div id="top-area">
       <header class="top-area inbox">
         <!-- 1-1. 로고박스 -->
@@ -33,18 +33,18 @@ const TopComp = Vue.component('top-comp',{
       </header>      
     </div>   
     `,
-    // 1-2. 데이터 셋업 리턴 메서드 /////
-    data(){
-        return{
-            // (1) GNB 메뉴 데이터
-            gnbMenu:['FASHION','BEAUTY','LIFESTYLE','CULTURE','VIDEO'],
-            // (2) 요약 메뉴 데이터
-            sumMenu:['KOREA','구독하기','≡'],
-        };
-    },
+  // 1-2. 데이터 셋업 리턴 메서드 /////
+  data() {
+    return {
+      // (1) GNB 메뉴 데이터
+      gnbMenu: ["FASHION", "BEAUTY", "LIFESTYLE", "CULTURE", "VIDEO"],
+      // (2) 요약 메뉴 데이터
+      sumMenu: ["KOREA", "구독하기", "≡"],
+    };
+  },
 });
 // 2. 하단컴포넌트
-const BottomComp = Vue.component('bottom-comp',{
+const BottomComp = Vue.component("bottom-comp", {
   // 템플릿 코드 ////
   template: `
     <div id="footer-area">
@@ -76,10 +76,10 @@ const BottomComp = Vue.component('bottom-comp',{
       </div>
   `,
   // 데이터 설정 ////
-  data(){
-    return{};
+  data() {
+    return {};
   },
 });
 
 // 3. 내보내기
-export {TopComp,BottomComp};
+export { TopComp, BottomComp };
