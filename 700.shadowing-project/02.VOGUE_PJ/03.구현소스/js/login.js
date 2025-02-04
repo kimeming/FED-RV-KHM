21ba35e963587606ad77d8dcf65a4a53edb06028
kimeming <kimeming030@gmail.com>
2025-02-04 16:29:03 +0900

    소스 현행화

---
+++ 700.shadowing-project/02.VOGUE_PJ/03.구현소스/js/login.js
@@ -0,0 +1,10 @@
+// 보그 JS 로그인 컴포넌트 - login.js
+
+export const LoginComp =  
+Vue.component("login-comp",{
+    template: ``,
+    data(){},
+    methods: {},
+    created(){},
+    mounted(){},
+});
